4be72cdff3f95493ef7c82a7b97143c6f962e3cb
Dennis Elser <patois@users.noreply.github.com>
2020-05-27 12:02:11 +0200

    Add files via upload

--- abyss_filters/item_index.py
+++ abyss_filters/item_index.py
@@ -4,11 +4,12 @@ import re
 
 def replace_addr_tags(s):
     tag = "%c%c" % (il.COLOR_ON, il.COLOR_ADDR)
+    tag_size = len(tag)
     ti = {}
     p = s.find(tag)
     while p != -1:
-        ti[s[p+2:p+2+il.COLOR_ADDR_SIZE]] = 0
-        p = s.find(tag, p+2+il.COLOR_ADDR_SIZE)
+        ti[s[p+tag_size:p+tag_size+il.COLOR_ADDR_SIZE]] = 0
+        p = s.find(tag, p+tag_size+il.COLOR_ADDR_SIZE)
     for addr in ti.keys():
         s = s.replace(tag+addr, il.COLSTR("<%s>" % addr, il.SCOLOR_AUTOCMT)+tag+addr)
     return s
