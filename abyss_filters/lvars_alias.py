3636008d7f93d9f96b8e6dddbc124ae87e5825b1
Dennis Elser <patois@users.noreply.github.com>
2020-06-05 11:35:40 +0200

    support typecast

--- abyss_filters/lvars_alias.py
+++ abyss_filters/lvars_alias.py
@@ -13,6 +13,7 @@ fDebug = False
 def debug(msg):
     if fDebug:
         print(msg)
+    return
 
 def debug_lvars(lvars):
     if not fDebug:
@@ -20,12 +21,14 @@ def debug_lvars(lvars):
     print("%d variables" % len(lvars))
     for one in lvars:
         print("%s %s; // %s" % (one.type(), one.name, one.cmt))
+    return
 
 def set_var_unique_name(var_x, var_y, lvars):
     old_name = var_x.name
     new_name = var_y.name + VAR_ASG_VAR_SUFFIX
     new_name = set_unique_name(var_x, new_name, lvars)
     debug("Renamed: %s (%s) = %s " % (old_name, new_name, var_y.name))
+    return
 
 def set_func_unique_name(var_x, func_name, lvars):
     old_name = var_x.name
@@ -36,6 +39,7 @@ def set_func_unique_name(var_x, func_name, lvars):
         new_name = VAR_ASG_CALL_PREFIX + func_name
     new_name = set_unique_name(var_x, new_name, lvars)
     debug("Renamed: %s (%s) = %s(...) " % (old_name, new_name, func_name))
+    return
 
 def set_unique_name(var_x, new_name, lvars):
     bExist = True
@@ -57,7 +61,8 @@ class asg_visitor_t(hr.ctree_visitor_t):
         self.cfunc = cfunc
         self.lvars = cfunc.get_lvars()
         debug_lvars(self.lvars)
-
+        return
+        
     def visit_expr(self, e):
         if e.op == hr.cot_asg:
             # is x a var?
@@ -86,17 +91,24 @@ class asg_visitor_t(hr.ctree_visitor_t):
                     else:
                         debug("Skipped: %s = %s " % (var_x.name, var_y.name))
                 # handle "x = y()" types of assignments
-                elif e.y.op == hr.cot_call and e.y.x.op == hr.cot_obj:
-                    # get name of called function
-                    func_name = ida_name.get_ea_name(e.y.x.obj_ea)
-                    # get var index and lvar_t
-                    x_idx = e.x.v.idx
-                    var_x = self.lvars[x_idx]
-                    if not var_x.has_user_name:
-                        # rename x
-                        set_func_unique_name(var_x, func_name, self.lvars)
-                    else:
-                        debug("Skipped: %s = %s(...) " % (var_x.name, func_name))
+                elif e.y.op in [hr.cot_call, hr.cot_cast]:
+                    if e.y.op == hr.cot_call:
+                        tmp_y = e.y
+                    elif e.y.op == hr.cot_cast and e.y.x.op == hr.cot_call:
+                        tmp_y = e.y.x
+                    else: # TBD
+                        tmp_y = None
+                    if tmp_y and tmp_y.x.op == hr.cot_obj:
+                        # get name of called function
+                        func_name = ida_name.get_ea_name(tmp_y.x.obj_ea)
+                        # get var index and lvar_t
+                        x_idx = e.x.v.idx
+                        var_x = self.lvars[x_idx]
+                        if not var_x.has_user_name:
+                            # rename x
+                            set_func_unique_name(var_x, func_name, self.lvars)
+                        else:
+                            debug("Skipped: %s = %s(...) " % (var_x.name, func_name))
         return 0
 
 
